cfb4222f8a7b3c1b56fa4f01484ba7dd5a318d53
Scott Erwin <scottallenerwin@gmail.com>
2021-03-27 16:34:54 -0400

    updated all code so its up to date with js and css and html also updated all the css so it scales with content better

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 var button = $('#button');
 var movie;
 
-
-
 button.on('click', function(event) {
     movie = $('#search').val();
     localStorage.setItem("searched-movie", JSON.stringify(movie));
