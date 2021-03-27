290db5f0b94c99fbbeea637bcea5d5c6833518df
elgalloloco <zreggert@gmail.com>
2021-03-27 12:43:23 -0400

    made the movie specs populate in the DOM

--- js/moviescript.js
+++ js/moviescript.js
@@ -1,19 +1,20 @@
 var movie;
 
-getapis(movie);
+$(document).ready(function(){
+    getapis(movie);
 
-function getapis(movie) {
-    var input = JSON.parse(localStorage.getItem("searched-movie"));
-    if (input === "") {
-        alert("Please a movie title");
+    function getapis(movie) {
+        var input = JSON.parse(localStorage.getItem("searched-movie"));
+        if (input === "") {
+            alert("Please a movie title");
     }
-    //console.log(input);
-    movie = encodeURIComponent(input);
-    //console.log(movie);
-    var url = `https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/${movie}`
+        //console.log(input);
+        movie = encodeURIComponent(input);
+        //console.log(movie);
+        var url = `https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/${movie}`
 
-    //call to get the poster, title, and movie id
-    fetch(url, {
+        //call to get the poster, title, and movie id
+        fetch(url, {
         "method": "GET",
         "headers": {
             "x-rapidapi-key": "d783afd0e7mshf20b2a536ca3e88p1e16fcjsnd4befaf76dd4",
@@ -53,11 +54,11 @@ function getapis(movie) {
         .catch(err => {
             console.error(err);
         });
-    getGifyApi();
-}
+        getGifyApi();
+    }
 
 // api call to get a gif related to the movie being searched
-function getGifyApi(movie) {
+    function getGifyApi(movie) {
 
     var gifyurl = `https://api.giphy.com/v1/gifs/random?api_key=xK1kP8TEjhDs45YiVK94I0DCP8b8N4ds&tag=${movie}&rating=pg`
         
@@ -68,39 +69,45 @@ function getGifyApi(movie) {
         .then(function (gifydata) {
             console.log(gifydata);
         });
-}
-
- function movieInfo(data) {
-     var movieId = data.titles[0].id;
-     var movieTitle = data.titles[0].title;
-     var poster = data.titles[0].image;
-//      console.log(movieId);
-//      console.log(movieTitle);
-//      console.log(poster);
-}
-
-
-function getCast(idData) {
-    for ( var i = 0; i < idData.cast.length; i++) {
-        var castMembers= idData.cast[i].actor;
-        //console.log(castMem);
-        var characters = idData.cast[i].character;
-        // console.log(castMembers);
-        // console.log(characters);
     }
-}
 
-function getMovieSpecs(idData) {
-    var plot = idData.plot;
-    console.log(plot);
-    var length = idData.length;
-    console.log(length);
-    var rating = idData.rating;
-    console.log(rating);
-    var ratingVotes = idData.rating_votes;
-    console.log(ratingVotes);
-    var trailerLink = idData.trailer.link;
-    console.log(trailerLink);
-    var year = idData.year;
-    console.log(year);
-}
+    function movieInfo(data) {
+        var movieTitle = data.titles[0].title;
+        var poster = data.titles[0].image;
+
+        
+        console.log(movieTitle);
+        console.log(poster);
+
+        $('.id-arr-title').text(movieTitle);
+        $('.id-arr-img').attr('src', poster);
+    }
+
+
+    function getCast(idData) {
+        for ( var i = 0; i < idData.cast.length; i++) {
+            var castMembers= idData.cast[i].actor;
+            //console.log(castMem);
+            var characters = idData.cast[i].character;
+            // console.log(castMembers);
+            // console.log(characters);
+        }   
+    }   
+
+    function getMovieSpecs(idData) {
+        var plot = idData.plot;
+        console.log(plot);
+        $('#info-plot').text(plot);
+        var rating = idData.rating;
+        console.log(rating);
+        $("#info-rating").append(rating)
+        var ratingVotes = idData.rating_votes;
+        console.log(ratingVotes);
+        $("#info-rating-votes").append(ratingVotes);
+        var trailerLink = idData.trailer.link;
+        console.log(trailerLink);
+        $("#info-trailer-link").prepend(`<a>${trailerLink}</a>`)
+        var year = idData.year;
+        console.log(year);  
+    }
+})
